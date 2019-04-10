5bf33ad1fcfdae7351514e1ed528c0b099f34baa
Armando Iglesias <31299078+armandoiglesias@users.noreply.github.com>
2019-04-10 16:21:03 -0400

    v1

---
+++ Form1.Designer.cs
@@ -0,0 +1,128 @@
+﻿namespace WindowsFormsApplication1
+{
+    partial class Form1
+    {
+        /// <summary>
+        /// Required designer variable.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Clean up any resources being used.
+        /// </summary>
+        /// <param name="disposing">true if managed resources should be disposed; otherwise, false.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Windows Form Designer generated code
+
+        /// <summary>
+        /// Required method for Designer support - do not modify
+        /// the contents of this method with the code editor.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.components = new System.ComponentModel.Container();
+            System.ComponentModel.ComponentResourceManager resources = new System.ComponentModel.ComponentResourceManager(typeof(Form1));
+            this.logsText = new System.Windows.Forms.TextBox();
+            this.notifyIcon1 = new System.Windows.Forms.NotifyIcon(this.components);
+            this.contextMenu = new System.Windows.Forms.ContextMenuStrip(this.components);
+            this.abrirToolStripMenuItem = new System.Windows.Forms.ToolStripMenuItem();
+            this.toolStripSeparator1 = new System.Windows.Forms.ToolStripSeparator();
+            this.salirToolStripMenuItem = new System.Windows.Forms.ToolStripMenuItem();
+            this.serialPort1 = new System.IO.Ports.SerialPort(this.components);
+            this.timer1 = new System.Windows.Forms.Timer(this.components);
+            this.contextMenu.SuspendLayout();
+            this.SuspendLayout();
+            // 
+            // logsText
+            // 
+            this.logsText.Enabled = false;
+            this.logsText.Location = new System.Drawing.Point(22, 31);
+            this.logsText.Multiline = true;
+            this.logsText.Name = "logsText";
+            this.logsText.Size = new System.Drawing.Size(275, 161);
+            this.logsText.TabIndex = 2;
+            // 
+            // notifyIcon1
+            // 
+            this.notifyIcon1.BalloonTipIcon = System.Windows.Forms.ToolTipIcon.Info;
+            this.notifyIcon1.BalloonTipText = "Prueba";
+            this.notifyIcon1.BalloonTipTitle = "Titulo";
+            this.notifyIcon1.ContextMenuStrip = this.contextMenu;
+            this.notifyIcon1.Icon = ((System.Drawing.Icon)(resources.GetObject("notifyIcon1.Icon")));
+            this.notifyIcon1.Text = "Bluetooth Client";
+            this.notifyIcon1.DoubleClick += new System.EventHandler(this.notifyIcon1_DoubleClick);
+            // 
+            // contextMenu
+            // 
+            this.contextMenu.Items.AddRange(new System.Windows.Forms.ToolStripItem[] {
+            this.abrirToolStripMenuItem,
+            this.toolStripSeparator1,
+            this.salirToolStripMenuItem});
+            this.contextMenu.Name = "fgffgfg";
+            this.contextMenu.Size = new System.Drawing.Size(101, 54);
+            // 
+            // abrirToolStripMenuItem
+            // 
+            this.abrirToolStripMenuItem.Name = "abrirToolStripMenuItem";
+            this.abrirToolStripMenuItem.Size = new System.Drawing.Size(100, 22);
+            this.abrirToolStripMenuItem.Text = "Abrir";
+            this.abrirToolStripMenuItem.Click += new System.EventHandler(this.abrirToolStripMenuItem_Click);
+            // 
+            // toolStripSeparator1
+            // 
+            this.toolStripSeparator1.Name = "toolStripSeparator1";
+            this.toolStripSeparator1.Size = new System.Drawing.Size(97, 6);
+            // 
+            // salirToolStripMenuItem
+            // 
+            this.salirToolStripMenuItem.Name = "salirToolStripMenuItem";
+            this.salirToolStripMenuItem.Size = new System.Drawing.Size(100, 22);
+            this.salirToolStripMenuItem.Text = "Salir";
+            this.salirToolStripMenuItem.Click += new System.EventHandler(this.salirToolStripMenuItem_Click);
+            // 
+            // serialPort1
+            // 
+            this.serialPort1.PortName = "COM2";
+            // 
+            // timer1
+            // 
+            this.timer1.Interval = 125;
+            this.timer1.Tick += new System.EventHandler(this.timer1_Tick);
+            // 
+            // Form1
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.ClientSize = new System.Drawing.Size(395, 280);
+            this.Controls.Add(this.logsText);
+            this.Name = "Form1";
+            this.Text = "Server Side";
+            this.Load += new System.EventHandler(this.Form1_Load);
+            this.Resize += new System.EventHandler(this.Form1_Resize);
+            this.contextMenu.ResumeLayout(false);
+            this.ResumeLayout(false);
+            this.PerformLayout();
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.TextBox logsText;
+        private System.Windows.Forms.NotifyIcon notifyIcon1;
+        private System.Windows.Forms.ContextMenuStrip contextMenu;
+        private System.Windows.Forms.ToolStripMenuItem abrirToolStripMenuItem;
+        private System.Windows.Forms.ToolStripSeparator toolStripSeparator1;
+        private System.Windows.Forms.ToolStripMenuItem salirToolStripMenuItem;
+        private System.IO.Ports.SerialPort serialPort1;
+        private System.Windows.Forms.Timer timer1;
+    }
+}
+
---
+++ Form1.cs
@@ -0,0 +1,180 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+
+using System.Threading;
+using System.Net.Sockets;
+
+//importing InTheHand library
+using InTheHand.Net.Bluetooth;
+using InTheHand.Windows.Forms;
+using InTheHand.Net.Sockets;
+using InTheHand.Net.Bluetooth.AttributeIds;
+using Libreria;
+using System.Runtime.InteropServices;
+using Ets2SdkClient;
+
+
+namespace WindowsFormsApplication1
+{
+    public partial class Form1 : Form
+    {
+
+        public Ets2SdkTelemetry Telemetry;
+        Ets2Telemetry _data;
+
+
+        //Thread AcceptAndListeningThread;
+        // helper variable
+        Boolean isConnected = false;
+        //bluetooth stuff
+        //BluetoothClient btClient;  //represent the bluetooth client
+        //connection
+        //BluetoothListener btListener; //represent this server bluetooth
+
+        public Form1()
+        {
+            InitializeComponent();
+        }
+
+        private void Form1_FormClosing(object sender, FormClosingEventArgs e)
+        {
+            try
+            {
+                if (serialPort1.IsOpen)
+                {
+                    serialPort1.Close();
+                }
+            }
+            catch (Exception ex)
+            {
+
+            }
+        }
+
+        private void Form1_Resize(object sender, EventArgs e)
+        {
+            //if the form is minimized  
+            //hide it from the task bar  
+            //and show the system tray icon (represented by the NotifyIcon control)  
+            if (this.WindowState == FormWindowState.Minimized)
+            {
+                //Hide();
+                notifyIcon1.Visible = true;
+            }
+           
+        }
+
+        private void notifyIcon1_DoubleClick(object sender, EventArgs e)
+        {
+            Show();
+            this.WindowState = FormWindowState.Normal;
+            notifyIcon1.Visible = false;
+        }
+
+        private void abrirToolStripMenuItem_Click(object sender, EventArgs e)
+        {
+            notifyIcon1_DoubleClick(null, null);
+        }
+
+        private void salirToolStripMenuItem_Click(object sender, EventArgs e)
+        {
+            Close();
+        }
+
+        private void Form1_Load(object sender, EventArgs e)
+        {
+
+            if (!serialPort1.IsOpen)
+            {
+                serialPort1.Open();
+            }
+
+            timer1.Interval = 200;
+            timer1.Enabled = true;
+
+            Telemetry = new Ets2SdkTelemetry();
+            Telemetry.Data += Telemetry_Data;
+
+            
+        }
+
+        private void Telemetry_Data(Ets2Telemetry data, bool updated)
+        {
+            try
+            {
+                if (this.InvokeRequired)
+                {
+                    this.Invoke(new TelemetryData(Telemetry_Data), new object[2] { data, updated });
+                    _data = data;
+                    return;
+                }
+                _data = data;
+            }
+            catch (Exception exception) {
+                notifyIcon1.ShowBalloonTip(450, "Error", exception.Message , ToolTipIcon.Error);
+            }
+        }
+
+
+        private void timer1_Tick(object sender, EventArgs e)
+        {
+            
+            if (serialPort1.IsOpen && _data != null)
+            {
+                if (!_data.Paused )
+            	{
+
+                    int luces = 0;
+                    if (_data.Lights.BlinkerRightOn)
+                    {
+                        luces += 32;
+                    }
+
+                    if (_data.Lights.BlinkerLeftOn)
+                    {
+                        luces += 16;
+                    }
+
+                    if (_data.Lights.LowBeams)
+                    {
+                        luces += 8;
+                    }
+
+                    if (_data.Lights.HighBeams)
+                    {
+                        luces += 4;
+                    }
+
+
+                    int lucesAdvertencia = 0;
+                    if (_data.Drivetrain.ParkingBrake)
+                    {
+                        lucesAdvertencia += 127;
+                    }
+
+                    if (_data.Drivetrain.FuelWarningLight     )
+                    {
+                        lucesAdvertencia += 8;
+                    }
+
+                    string _mensaje = _data.Physics.SpeedKmh +   _data.Drivetrain.EngineRpm + "00000000" + luces + lucesAdvertencia + "00" ; 
+                    logsText.Text += _mensaje + Environment.NewLine;
+
+		            serialPort1.Write(0xFF + "2" +  _mensaje );
+	            }
+            }
+            else {
+                notifyIcon1.ShowBalloonTip(450, "Error", "Error al intentar enviar por puerto serial", ToolTipIcon.Error);
+            }
+        }
+
+    }
+
+}
